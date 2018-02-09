95de97dfbb63d5bb3865d3608e0c1ea9526c20b0
mojtaba <ramezanimojtaba72@gmail.com>
2018-02-09 03:40:41 +0330

    fix: Repairing syntax in problem1

--- problem1.java
+++ problem1.java
@@ -1,11 +1,9 @@
 public class problem1 {
     public static void main(String[] args) {
         int sum = 0, n = 1000;
-        for(int i = 0; i < n; i++) {
-            if(i % 3 == 0 || i % 5 == 0) {
+        for(int i = 0; i < n; i++)
+            if(i % 3 == 0 || i % 5 == 0)
                 sum += i;
-            }
-        }
         System.out.println(sum);
     }
 }
