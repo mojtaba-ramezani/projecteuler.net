7ac849a86f602d02d943a2dc6500717ff0246c1f
mojtaba <ramezanimojtaba72@gmail.com>
2018-03-24 12:16:19 +0430

    add: Solving problem10, fix: Repairing problem9 syntax

---
+++ problem10.java
@@ -0,0 +1,15 @@
+public class problem10 {
+    public static void main(String[] args) {
+        long sum = 2;
+        for (int i = 3; i < 2000000; i += 2)
+            if (isPrime(i)) sum += i;
+        System.out.println(sum);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2)
+            if (n % i == 0) return false;
+        return true;
+    }
+}
